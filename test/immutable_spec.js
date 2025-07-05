dfe113f8b925b683cdc52947afe48efc7dbc7fe8
jamesvo2103 <vongochieu2103@gmail.com>
2025-07-04 20:56:25 -0400

    state tree

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,19 +1,38 @@
 import {expect} from 'chai';
+import {List, Map} from 'immutable';
 
 describe('immutability', () => {
 
-  describe('a number', () => {
+  // ...
 
-    function increment(currentState) {
-      return currentState + 1;
+  describe('a tree', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      );
     }
 
     it('is immutable', () => {
-      let state = 42;
-      let nextState = increment(state);
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addMovie(state, 'Sunshine');
 
-      expect(nextState).to.equal(43);
-      expect(state).to.equal(42);
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
     });
 
   });
